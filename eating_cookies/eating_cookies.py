9a328ff22eff77845523efce0b01a1abc06df986
mrsteveson <patrick.steveson727@gmail.com>
2019-10-10 15:26:18 -0500

    reworked my eating_cookies solution to use caching

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -11,7 +11,27 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  if n < 0:
+    return 0
+  elif n == 0:
+    return 1
+  elif n <= 2:
+    return n
+  elif cache and cache[n] > 0:
+    return cache[n]
+  else:
+    if not cache:
+      cache = {i: 0 for i in range(n + 1)}
+    cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+    return cache[n]
+  
+  # Without caching this brute force type solution takes far too long to answer the test cases.
+  # if n == 0:
+  #   return 1
+  # elif n <= 2:
+  #   return n
+  # else:
+  #   return(eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
